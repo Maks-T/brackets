fb240c3650628878c0409992eecd26fe362e8c78
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2021-02-24 14:41:02 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-module.exports = module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
  let brackets = [];
     bracketsConfig.forEach(item => {
       brackets.push(item.join(''));      
   });
 
-  for(i=0; i<brackets.length; i++) {
+  for(let i=0; i<brackets.length; i++) {
     if (str.includes(brackets[i])) {
-      str.replace(brackets[i],'');
+      str = str.replace(brackets[i],'');
       i = -1;
     }     
   }
 
   return str.length === 0;
    
-}
-
+};
