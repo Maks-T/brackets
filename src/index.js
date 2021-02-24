ea496a7d2727d3f6d771196f23889661b69c7e52
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2021-02-24 14:03:28 +0300

    feat: add subtask brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = module.exports = function check(str, bracketsConfig) {
+ let brackets = [];
+    bracketsConfig.forEach(item => {
+      brackets.push(item.join(''));      
+  });
+
+  for(i=0; i<brackets.length; i++) {
+    if (str.includes(brackets[i])) {
+      str.replace(brackets[i],'');
+      i = -1;
+    }     
+  }
+
+  return str.length === 0;
+   
 }
+
